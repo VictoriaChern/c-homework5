abe0d08f81c9c88351584f9f0d2bed5294efa9dd
Viktoria.Cherniavskaya <victoria.kli1707@gmail.com>
2022-05-02 00:38:08 +0300

    add homework 5

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+int[] array = randomArray(4);
+WriteLine($"[{string.Join(",",array)}] -> {sumOfOdd(array)}");
+
+int[] randomArray(int size) {
+    Random random = new Random();
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++) {
+        array[i] = random.Next(-10,100);
+    }
+    return array;
+}
+
+int sumOfOdd(int[] array) {
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++) {
+        if(i % 2 != 0) {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
